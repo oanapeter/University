c149e71fa499cdf7bab007f00bac435c30ee122b
oanapeter <115104535+oanapeter@users.noreply.github.com>
2024-05-06 12:01:49 +0300

    Add files via upload

---
+++ Second year /Second semester/Database Management Systems/Lab1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace a1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parentTable = new System.Windows.Forms.DataGridView();
+            this.childTable = new System.Windows.Forms.DataGridView();
+            this.attendanceID = new System.Windows.Forms.Label();
+            this.employeeID = new System.Windows.Forms.Label();
+            this.workedHours = new System.Windows.Forms.Label();
+            this.attendanceIDTextBox = new System.Windows.Forms.TextBox();
+            this.employeeIDTextBox = new System.Windows.Forms.TextBox();
+            this.workedHoursTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parentTable
+            // 
+            this.parentTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parentTable.Location = new System.Drawing.Point(13, 23);
+            this.parentTable.Name = "parentTable";
+            this.parentTable.Size = new System.Drawing.Size(450, 184);
+            this.parentTable.TabIndex = 0;
+            this.parentTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.parentTable_CellClick);
+            // 
+            // childTable
+            // 
+            this.childTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.childTable.Location = new System.Drawing.Point(497, 23);
+            this.childTable.Name = "childTable";
+            this.childTable.Size = new System.Drawing.Size(440, 184);
+            this.childTable.TabIndex = 1;
+            this.childTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.childTable_CellClick);
+            // 
+            // attendanceID
+            // 
+            this.attendanceID.AutoSize = true;
+            this.attendanceID.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attendanceID.Location = new System.Drawing.Point(13, 246);
+            this.attendanceID.Name = "attendanceID";
+            this.attendanceID.Size = new System.Drawing.Size(156, 29);
+            this.attendanceID.TabIndex = 2;
+            this.attendanceID.Text = "AttendanceID";
+            // 
+            // employeeID
+            // 
+            this.employeeID.AutoSize = true;
+            this.employeeID.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeID.Location = new System.Drawing.Point(13, 306);
+            this.employeeID.Name = "employeeID";
+            this.employeeID.Size = new System.Drawing.Size(145, 29);
+            this.employeeID.TabIndex = 3;
+            this.employeeID.Text = "EmployeeID";
+            // 
+            // workedHours
+            // 
+            this.workedHours.AutoSize = true;
+            this.workedHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workedHours.Location = new System.Drawing.Point(13, 362);
+            this.workedHours.Name = "workedHours";
+            this.workedHours.Size = new System.Drawing.Size(161, 29);
+            this.workedHours.TabIndex = 4;
+            this.workedHours.Text = "WorkedHours";
+            // 
+            // attendanceIDTextBox
+            // 
+            this.attendanceIDTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attendanceIDTextBox.Location = new System.Drawing.Point(200, 246);
+            this.attendanceIDTextBox.Name = "attendanceIDTextBox";
+            this.attendanceIDTextBox.Size = new System.Drawing.Size(263, 31);
+            this.attendanceIDTextBox.TabIndex = 5;
+            // 
+            // employeeIDTextBox
+            // 
+            this.employeeIDTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeIDTextBox.Location = new System.Drawing.Point(200, 306);
+            this.employeeIDTextBox.Name = "employeeIDTextBox";
+            this.employeeIDTextBox.Size = new System.Drawing.Size(263, 31);
+            this.employeeIDTextBox.TabIndex = 6;
+            // 
+            // workedHoursTextBox
+            // 
+            this.workedHoursTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workedHoursTextBox.Location = new System.Drawing.Point(200, 362);
+            this.workedHoursTextBox.Name = "workedHoursTextBox";
+            this.workedHoursTextBox.Size = new System.Drawing.Size(263, 31);
+            this.workedHoursTextBox.TabIndex = 7;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(660, 221);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(132, 45);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.Location = new System.Drawing.Point(660, 286);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(132, 49);
+            this.deleteButton.TabIndex = 9;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.Location = new System.Drawing.Point(660, 352);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(132, 48);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 430);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.workedHoursTextBox);
+            this.Controls.Add(this.employeeIDTextBox);
+            this.Controls.Add(this.attendanceIDTextBox);
+            this.Controls.Add(this.workedHours);
+            this.Controls.Add(this.employeeID);
+            this.Controls.Add(this.attendanceID);
+            this.Controls.Add(this.childTable);
+            this.Controls.Add(this.parentTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.parentTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView parentTable;
+        private System.Windows.Forms.DataGridView childTable;
+        private System.Windows.Forms.Label attendanceID;
+        private System.Windows.Forms.Label employeeID;
+        private System.Windows.Forms.Label workedHours;
+        private System.Windows.Forms.TextBox attendanceIDTextBox;
+        private System.Windows.Forms.TextBox employeeIDTextBox;
+        private System.Windows.Forms.TextBox workedHoursTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ Second year /Second semester/Database Management Systems/Lab1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection dbConnection;
+        private SqlDataAdapter adapter;
+        private DataSet dataSet;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConnection = new SqlConnection("Data Source = DESKTOP-R01UH5Q\\SQLEXPRESS; Initial Catalog = MakeUpShop; Integrated Security = True; TrustServerCertificate=True;");
+            adapter = new SqlDataAdapter();
+            dataSet = new DataSet();
+
+            parentTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            parentTableLoad();
+
+        }
+
+        private void parentTableLoad()
+        {
+            adapter.SelectCommand = new SqlCommand("select * from Employees", dbConnection);
+            dataSet.Clear();
+            adapter.Fill(dataSet, "Employees");
+            parentTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            parentTable.DataSource = dataSet.Tables["Employees"];
+
+
+
+        }
+
+        private void childTableLoad()
+        {
+            dataSet = new DataSet();
+            adapter.Fill(dataSet, "EmployeeAttendance");
+            childTable.DataSource = dataSet.Tables["EmployeeAttendance"];
+            childTable.SelectionMode= DataGridViewSelectionMode.FullRowSelect;
+
+        }
+
+        private void clearTextBoxes()
+        {
+            attendanceIDTextBox.Clear();
+            employeeIDTextBox.Clear();
+            workedHoursTextBox.Clear();
+        }
+
+
+
+
+
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                adapter.InsertCommand = new SqlCommand("insert into EmployeeAttendance(AttendanceID, EmployeeID, WorkedHours) values (@aid, @eid, @hours)", dbConnection);
+  
+                adapter.InsertCommand.Parameters.Add("@aid", SqlDbType.Int).Value = Int32.Parse(attendanceIDTextBox.Text);
+                adapter.InsertCommand.Parameters.Add("@eid", SqlDbType.Int).Value = Int32.Parse(employeeIDTextBox.Text);
+
+                adapter.InsertCommand.Parameters.Add("@hours", SqlDbType.Int).Value = Int32.Parse(workedHoursTextBox.Text);
+
+                dbConnection.Open();
+                adapter.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("Inserted successfully in the database");
+                dbConnection.Close();
+
+                parentTableLoad();
+                clearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dbConnection.Close();
+            }
+
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = childTable.SelectedRows[0].Index;
+                DialogResult dialogResult;
+                dialogResult = MessageBox.Show("Are you sure?", "Please confirm the deletion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    adapter.DeleteCommand = new SqlCommand("delete from EmployeeAttendance where AttendanceID = @id", dbConnection);
+                    adapter.DeleteCommand.Parameters.Add("@id", SqlDbType.Int).Value = Int32.Parse(attendanceIDTextBox.Text);
+                    dbConnection.Open();
+                    adapter.DeleteCommand.ExecuteNonQuery();
+                    MessageBox.Show("Successfully deleted from database", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    dbConnection.Close();
+
+                    childTableLoad();
+                    clearTextBoxes();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dbConnection.Close();
+            }
+
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = childTable.SelectedRows[0].Index;
+                adapter.UpdateCommand = new SqlCommand("update EmployeeAttendance set EmployeeID = @eid, WorkedHours = @hours where AttendanceID = @aid", dbConnection);
+                adapter.UpdateCommand.Parameters.Add("@aid", SqlDbType.Int).Value = Int32.Parse(attendanceIDTextBox.Text);
+                adapter.UpdateCommand.Parameters.Add("@hours", SqlDbType.Int).Value = Int32.Parse(workedHoursTextBox.Text);
+                adapter.UpdateCommand.Parameters.Add("@eid", SqlDbType.Int).Value = Int32.Parse(employeeIDTextBox.Text);
+
+                dbConnection.Open();
+                adapter.UpdateCommand.ExecuteNonQuery();
+                MessageBox.Show("Updated succesfull", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dbConnection.Close();
+
+                childTableLoad();
+                clearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dbConnection.Close();
+            }
+        }
+
+        private void parentTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow selected = parentTable.SelectedRows[0];
+            if (selected.Cells[0].Value.ToString() != String.Empty)
+            {
+                employeeIDTextBox.Text = selected.Cells[0].Value.ToString();
+
+                int employeeID = Convert.ToInt32(selected.Cells[0].Value);
+                adapter.SelectCommand = new SqlCommand("select * from EmployeeAttendance where EmployeeID = @eid", dbConnection);
+                adapter.SelectCommand.Parameters.AddWithValue("@eid", employeeID);
+
+                childTableLoad();
+
+
+            }
+        }
+
+        private void childTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (childTable.SelectedRows.Count > 0)
+            {
+                int index = childTable.SelectedRows[0].Index;
+
+                if (index < dataSet.Tables["EmployeeAttendance"].Rows.Count)
+                {
+                    attendanceIDTextBox.Text = dataSet.Tables["EmployeeAttendance"].Rows[index][0].ToString();
+                    employeeIDTextBox.Text = dataSet.Tables["EmployeeAttendance"].Rows[index][1].ToString();
+                    workedHoursTextBox.Text = dataSet.Tables["EmployeeAttendance"].Rows[index][2].ToString();
+                }
+            }
+        }
+    }
+}
+
