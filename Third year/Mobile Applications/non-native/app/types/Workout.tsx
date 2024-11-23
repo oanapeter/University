dbb87d167a7f8a1b7e1bb7c75c0a9a17ef97c051
oanapeter <115104535+oanapeter@users.noreply.github.com>
2024-11-23 21:46:42 +0200

    Delete Third year/Mobile Applications directory

--- Third year/Mobile Applications/non-native/app/types/Workout.tsx
+++
@@ -1,8 +0,0 @@
-export type Workout = {
-    id: number;
-    name: string;
-    description: string;
-    duration: number;
-    intensity: 'Low' | 'Medium' | 'High';
-    burnedCalories: number;
-  };
